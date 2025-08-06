5ddfe814de06001514e869a4a5a664dd66e4da52
Ashley <ashleyclairetanton@icloud.com>
2025-08-06 11:23:25 +0200

    put in intro video
    need to test

--- CLONE_1_GROUP_8/Assets/scripts/gameManager.cs
+++ CLONE_1_GROUP_8/Assets/scripts/gameManager.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    public GameObject canvas;
+    public VideoPlayer videoPlayer;
+
+    public void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
     public void playGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -19,4 +28,16 @@ public class gameManager : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(2);
     }
+
+    public void introClip()
+    {
+        canvas.SetActive(false);
+        videoPlayer.Play();
+    }
+
+    void OnVideoEnd(UnityEngine.Video.VideoPlayer vp)
+    {
+         SceneManager.LoadSceneAsync(1);
+    }
+
 }
